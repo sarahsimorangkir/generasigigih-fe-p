3882f34a10ce192ad7e4f515f325d7e4ff3a9077
Sarah Simorangkir <sarahsimorangkir34@gmail.com>
2022-04-08 15:06:56 +0700

    add select unselect state

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
   const [token, setToken] = useState("");
   const [searchSong, setSearchSong] = useState("");
   const [songData, setSongData] = useState([]);
+  const [selectedSongs, setSelectedSongs] = useState([]);
+  const [combineSongs, setCombineSongs] = useState([]);
 
   useEffect(() => {
     const queryString = new URL(window.location.href.replace("#", "?"))
@@ -15,6 +17,16 @@ function App() {
     setToken(accessToken);
   }, []);
 
+  //pass songData for combineSong and add isSelected
+  useEffect(() => {
+    const handleCombineTracks = songData.map((song) => ({
+      ...song,
+      isSelected: selectedSongs.find((data) => data === song.uri),
+    }));
+    setCombineSongs(handleCombineTracks);
+  }, [songData, selectedSongs]);
+
+  //get song data from spotify
   const getSong = async () => {
     await axios
       .get(
@@ -27,6 +39,15 @@ function App() {
         console.log(error);
       });
   };
+
+  //handle select state of the songs
+  const handleSelect = (uri) => {
+    const selected = selectedSongs.find((song) => song === uri);
+    selected
+      ? setSelectedSongs(selectedSongs.filter((song) => song !== uri))
+      : setSelectedSongs([...selectedSongs, uri]);
+  };
+
   return (
     <div className="p-5 bg-gray-900 h-screen space-y-5 overflow-auto">
       <div className="text-center">
@@ -40,18 +61,18 @@ function App() {
           Login
         </a>
       </div>
-      <div class="flex justify-center">
-        <div class="mb-3 xl:w-96">
-          <div class="flex w-full mb-4">
+      <div className="flex justify-center">
+        <div className="mb-3 xl:w-96">
+          <div className="flex w-full mb-4">
             <input
               type="search"
-              class="flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal bg-white border border-solid border-gray-300 rounded-l transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              className="flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal bg-white border border-solid border-gray-300 rounded-l transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
               placeholder="Search"
               aria-label="Search"
               onChange={(e) => setSearchSong(e.target.value)}
             />
             <button
-              class="px-6 py-2 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-r focus:outline-none focus:ring-0 transition duration-150 ease-in-out hover:bg-blue-700"
+              className="px-6 py-2 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-r focus:outline-none focus:ring-0 transition duration-150 ease-in-out hover:bg-blue-700"
               type="button"
               onClick={getSong}
             >
@@ -61,14 +82,17 @@ function App() {
         </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-        {songData.map((song) => {
-          const { id, name, artists, album } = song;
+        {combineSongs.map((song) => {
+          const { uri, name, artists, album, isSelected } = song;
           return (
             <Song
-              key={id}
+              key={uri}
+              uri={uri}
               image={album.images[0]?.url}
               title={name}
               album={artists[0]?.name}
+              selectState={handleSelect}
+              isSelected={isSelected}
             />
           );
         })}

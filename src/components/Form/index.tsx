bb1f69b1a43ed061db8a131b4dcbc9932a37902b
Sarah Simorangkir <sarahsimorangkir34@gmail.com>
2022-04-22 19:36:30 +0700

    update alert

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState, useRef } from "react";
 import { useAppSelector } from "hooks/hooks";
 import {
   retrieveUserId,
@@ -14,19 +14,29 @@ import {
   FormLabel,
   Input,
   useToast,
+  useDisclosure,
 } from "@chakra-ui/react";
-import Dialog from "components/Dialog";
+import {
+  AlertDialog,
+  AlertDialogOverlay,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogCloseButton,
+  AlertDialogBody,
+  AlertDialogFooter,
+} from "@chakra-ui/react";
 
 const Form = ({ songUris }: songUrisInterface) => {
   const token = useAppSelector((state) => state.token.value);
   const [playlistId, setPlaylistId] = useState("");
   const [userId, setUserId] = useState("");
-  const [dialogOpen, setDialogOpen] = useState(false);
   const [form, setForm] = useState({
     title: "",
     description: "",
   });
   const toast = useToast();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = useRef<HTMLButtonElement>(null);
 
   //run addSong function when playlistId is set
   useEffect(() => {
@@ -70,10 +80,12 @@ const Form = ({ songUris }: songUrisInterface) => {
         .then((response) => {
           setPlaylistId(response.data.id);
         })
+        .then(() => {
+          onOpen();
+        })
         .catch((error) => {
           console.log(error);
         });
-        setDialogOpen(true);
       setForm({ title: "", description: "" });
       alert("Successfully created playlist");
     } else {
@@ -89,39 +101,67 @@ const Form = ({ songUris }: songUrisInterface) => {
 
   return (
     <>
-    <Center>
-      <Box w="sm">
-        <form onSubmit={handleSubmit}>
-          <FormControl mb="3">
-            <FormLabel htmlFor="title">Title</FormLabel>
-            <Input
-              type="text"
-              placeholder="Playlist Title"
-              id="title"
-              name="title"
-              value={form.title}
-              onChange={handleForm}
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor="title">Description</FormLabel>
-            <Input
-              type="text"
-              placeholder="Playlist Description"
-              id="description"
-              name="description"
-              value={form.description}
-              onChange={handleForm}
-            />
-          </FormControl>
-          <Button mt="3" w="100%" id="submit" type="submit" colorScheme="green">
-            Create
-          </Button>
-        </form>
-      </Box>
-    </Center>
-     <Dialog total={songUris.length} showConfirmation={dialogOpen} />
-     </>
+      <Center>
+        <Box w="sm">
+          <form onSubmit={handleSubmit}>
+            <FormControl mb="3">
+              <FormLabel htmlFor="title">Title</FormLabel>
+              <Input
+                type="text"
+                placeholder="Playlist Title"
+                id="title"
+                name="title"
+                value={form.title}
+                onChange={handleForm}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="title">Description</FormLabel>
+              <Input
+                type="text"
+                placeholder="Playlist Description"
+                id="description"
+                name="description"
+                value={form.description}
+                onChange={handleForm}
+              />
+            </FormControl>
+            <Button
+              mt="3"
+              w="100%"
+              id="submit"
+              type="submit"
+              colorScheme="green"
+            >
+              Create
+            </Button>
+          </form>
+        </Box>
+      </Center>
+      <AlertDialog
+        motionPreset="slideInBottom"
+        onClose={onClose}
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        isCentered
+      >
+        <AlertDialogOverlay />
+        <AlertDialogContent>
+          <AlertDialogHeader fontSize="lg" fontWeight="bold">
+            Successfully Created A Playlist!
+          </AlertDialogHeader>
+          <AlertDialogCloseButton />
+          <AlertDialogBody>
+            You've added {songUris.length} songs to your playlist!
+          </AlertDialogBody>
+          <AlertDialogFooter>
+            <Button onClick={onClose} colorScheme="green">
+              Okay
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   );
 };
 
